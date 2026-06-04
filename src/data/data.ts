23e268e3dd4f45f59b5bd43c8e7fdf13f8478477
M1g3l14Ka <katcion00@mail.ru>
2026-06-04 07:55:55 +0300

    add headerLinks for links in Header.tsx

---
+++ src/data/data.ts
@@ -0,0 +1,10 @@
+import { HederLink } from "@/types/types"
+
+export const headerLinks:HederLink[] = [
+    { id: 1, name: "Home", href: "/" },
+    { id: 2, name: "Contacts", href: "/service/contacts" },
+    { id: 3, name: "Policy", href: "/service/policy" },
+    { id: 4, name: "Favorites", href: "/user/favorites" },
+    { id: 5, name: "Orders", href: "/user/order" },
+    { id: 6, name: "Cart", href: "/user/cart" },
+]
