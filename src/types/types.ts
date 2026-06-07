c2ede65f8e8b01977a29b0e77bce19fad6b50321
M1g3l14Ka <katcion00@mail.ru>
2026-06-07 03:54:36 +0300

    add types for products for Main.tsx

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,7 +1,31 @@
 
 
-export interface HederLink {
+export interface HeaderLink {
     id: number;
     name: string;
     href: string;
-}
+};
+
+export interface DummyJsonProducts {
+    id: number;
+    title: string;
+    description: string;
+    thumbnail: string;
+    category: string;
+    price: number;
+    discountPercentage: number;
+    rating: number;
+    stock: number;
+    tags: [
+        string
+    ];
+    brand: string;
+    reviews: [
+        rating: number,
+        comment: string,
+        data: Date,
+        reviewerName: string,        
+        reviewerEmail: string
+    ];
+    returnPolicy: string;
+};
