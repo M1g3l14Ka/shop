62e9bd6f0496e23cbb50ae0cf339093c05e87f36
M1g3l14Ka <katcion00@mail.ru>
2026-06-07 03:50:11 +0300

    change Home() to async and add products for Main.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,18 @@ import Main from "@/components/Main";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 
+import { Product } from "@/data/products";
 
-export default function Home() {
+export default async function Home() {
+
+  const products = await Product();
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center">
 
       <Hero/>
 
-      <Main/>
+      <Main products={products}/>
       
       <Footer/>
 
